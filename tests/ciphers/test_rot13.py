3c4fa98792fd14331c5af9d3187c3a2b21e4dcef
Pocholo <plpantoja49@gmail.com>
2020-07-07 20:57:52 +0800

    test: add tests for ROT13

---
+++ tests/ciphers/test_rot13.py
@@ -0,0 +1,13 @@
+from classicrack.ciphers import ROT13
+import unittest
+
+class TestCommon(unittest.TestCase):
+
+    def test_encode(self):
+        self.assertEqual(ROT13().encode('cheemsburmger'), 'purrzfoheztre')
+    
+    def test_decode(self):
+        self.assertEqual(ROT13().decode('purrzfoheztre'), 'cheemsburmger')
+
+if __name__ == '__main__':
+    unittest.main()
