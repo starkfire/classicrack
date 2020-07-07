85bbcafde47ab55725252ab5268b6dadf22825f7
Pocholo <plpantoja49@gmail.com>
2020-07-07 19:57:49 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='classicrack',
+    version='0.1.0',
+    description='Python library for implementing and cracking classical ciphers.',
+    url='https://github.com/starkfire/classicrack',
+    author='Pocholo Pantoja',
+    license='MIT',
+    packages=find_packages()
+)
