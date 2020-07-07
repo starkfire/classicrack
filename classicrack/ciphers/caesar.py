747232fc9fcdc3b8aa087c7bcf4a18878709c99b
Pocholo <plpantoja49@gmail.com>
2020-07-07 22:12:54 +0800

    feat: add cracking strategies for caesar

--- classicrack/ciphers/caesar.py
+++ classicrack/ciphers/caesar.py
@@ -1,4 +1,7 @@
-from classicrack.utils.common import shift_constant
+from classicrack.utils.common import *
+from classicrack.utils.ngrams import *
+
+from classicrack.strategies import frequency_analysis as fa
 
 class Caesar:
     """
@@ -12,3 +15,19 @@ class Caesar:
     
     def decode(self, text: str, shift: int):
         return shift_constant(text.lower().replace(' ', ''), shift, decode=True)
+
+    def crack_bruteforce(self, text: str):
+        values = [shift_constant(text, x, decode=True) for x in range(26)]
+        return values
+    
+    def crack_ngram(self, text: str, n: int = 1):
+        # TODO: function will sometimes return the correct value shifted one step backwards when n > 1
+        text = text.lower().replace(' ', '')
+
+        fa_strategy = {
+            1: fa.use_unigram(text),
+            2: fa.use_bigram(text),
+            3: fa.use_trigram(text),
+        }
+
+        return fa_strategy.get(n, 'Invalid or unsupported n-value')
