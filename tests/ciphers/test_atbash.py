8bfbef18388255490f6c7f1663b6bc1fb41e7ca9
Pocholo <plpantoja49@gmail.com>
2020-07-07 20:52:13 +0800

    test: add tests for atbash

---
+++ tests/ciphers/test_atbash.py
@@ -0,0 +1,13 @@
+from classicrack.ciphers import Atbash
+import unittest
+
+class TestCommon(unittest.TestCase):
+
+    def test_encode(self):
+        self.assertEqual(Atbash().encode('zebras'), 'avyizh')
+    
+    def test_decode(self):
+        self.assertEqual(Atbash().decode('avyizh'), 'zebras')
+
+if __name__ == '__main__':
+    unittest.main()
