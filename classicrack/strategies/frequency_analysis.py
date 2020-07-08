dab242d68ed33e499fb9a8d1a5483214f6cc37ba
Pocholo <plpantoja49@gmail.com>
2020-07-08 17:35:54 +0800

    style: add descriptions to methods

--- classicrack/strategies/frequency_analysis.py
+++ classicrack/strategies/frequency_analysis.py
@@ -2,12 +2,21 @@ from classicrack.utils.common import *
 from classicrack.utils.ngrams import *
 
 def use_unigram(text: str):
+    """
+    Cracks an input ciphertext by taking its most frequent letter, and its distance to letter 'e',
+    which is the most frequently used letter in the english alphabet.
+        text (str): ciphertext
+    """
     text = text.lower().replace(' ', '')
     most_frequent = most_frequent_char(text)
     shift = distance(most_frequent['char'], 'e')
     return shift_constant(text, shift)
 
 def use_bigram(text: str):
+    """
+    Cracks an input ciphertext by taking its bigrams and checking for shifted combinations of the bigram 'th'.
+        text (str): ciphertext
+    """
     text = text.lower().replace(' ', '')
     ngrams = get_ngrams(text, 2)
     bigrams = common_bigrams()
@@ -19,6 +28,10 @@ def use_bigram(text: str):
         return None
 
 def use_trigram(text: str):
+    """
+    Cracks an input ciphertext by taking its trigrams and checking for shifted combinations of the trigram 'the'.
+        text (str): ciphertext
+    """
     text = text.lower().replace(' ', '')
     ngrams = get_ngrams(text, 3)
     trigrams = common_trigrams()
